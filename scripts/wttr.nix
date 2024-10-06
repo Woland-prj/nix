3205fcb4e5c357de325b12dd7ccdac15c2d6c0e6
Woland-prj <nzimin93@gmail.com>
2024-10-06 22:04:33 +0300

    wttr script

--- scripts/wttr.nix
+++ scripts/wttr.nix
@@ -95,8 +95,9 @@ pkgs.writers.writePython3Bin "wttr" {
 
 
   data['alt'] = ''
-  data['text'] = WEATHER_CODES[weather['current_condition'][0]['weatherCode']] + \
-  " " + weather['current_condition'][0]['temp_C'] + "°"
+  data['text'] = \
+      WEATHER_CODES[weather['current_condition'][0]['weatherCode']] + \
+      " " + weather['current_condition'][0]['temp_C'] + "°"
 
   data['tooltip'] = f"<b>\
       {weather['current_condition'][0]['weatherDesc'][0]['value']} \
