2ddd4dd72641c4a87e0ca44adaf7b2d59b6ef7b8
Woland-prj <nzimin93@gmail.com>
2024-10-15 20:33:37 +0300

    cursor

--- home-manager/modules/cursor.nix
+++ home-manager/modules/cursor.nix
@@ -2,7 +2,7 @@
   home = {
     pointerCursor = {
       package = pkgs.rose-pine-cursor;
-      name = "rose-pine-cursor";
+      name = "BreezeX-RosePine-Linux";
       size = 24;
       gtk.enable = true;
       x11 = {
