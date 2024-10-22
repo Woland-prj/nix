6d067b78ef0d397a9cca90aed33d73b0e542bb5a
Woland-prj <nzimin93@gmail.com>
2024-10-22 09:55:49 +0300

    stylix

--- flake.nix
+++ flake.nix
@@ -25,37 +25,68 @@
     home-manager,
     ...
   } @ inputs: let
-    system = "x86_64-linux";
+    # ---- SYSTEM SETTINGS ---- #
+    systemSettings = {
+      system = "x86_64-linux"; # system arch
+      hostname = "wldnixos"; # hostname
+      profile = "personal"; # select a profile defined from my profiles directory
+      timezone = "Europe/Moscow"; # select timezone
+      locale = "en_US.UTF-8"; # select locale
+      bootMode = "uefi"; # uefi or bios
+      bootMountPath = "/boot"; # mount path for efi boot partition; only used for uefi boot mode
+      grubDevice = ""; # device identifier for grub; only used for legacy (bios) boot mode
+      gpuType = "nvidia"; # amd, intel or nvidia; only makes some slight mods for amd at the moment
+    };
+
+    # ----- USER SETTINGS ----- #
+    userSettings = {
+      username = "woland"; # username
+      name = "Woland"; # name/identifier
+      email = "nzimin93@gmail.com"; # email (used for certain configurations)
+      flakeDir = "~/nix"; # absolute path of the local repo
+      theme = "catppuccin-mocha"; # selcted theme from my themes directory (./themes/)
+      wm = "hyprland"; # Selected window manager or desktop environment; must select one in both ./user/wm/ and ./system/wm/
+      # window manager type (hyprland or x11) translator
+      term = "alacritty"; # Default terminal command;
+      font = "JetBrains Mono"; # Selected font
+      fontPkg = nixpkgs.jetbrains-mono; # Font package
+      editor = "nvim"; # Default editor;
+    };
+    system = systemSettings.system;
     pkgs-stable = import nixpkgs-stable {
       inherit system;
       config.allowUnfree = true;
     };
+    pkgs =
+      import nixpkgs
+      {
+        inherit system;
+        config.allowUnfree = true;
+      };
   in {
-    nixosConfigurations.wldnixos = nixpkgs.lib.nixosSystem {
+    nixosConfigurations.wldnixos = pkgs.lib.nixosSystem {
       specialArgs = {
         inherit pkgs-stable;
-        inherit inputs system;
+        inherit userSettings;
+        inherit systemSettings;
+        inherit inputs;
       };
 
       modules = [
         ./nixos/configuration.nix
-        inputs.stylix.nixosModules.stylix
       ];
     };
 
     homeConfigurations.woland = home-manager.lib.homeManagerConfiguration {
-      pkgs =
-        import nixpkgs
-        {
-          inherit system;
-          config.allowUnfree = true;
-        };
+      inherit pkgs;
       modules = [
         ./home-manager/home.nix
-        inputs.nixvim.homeManagerModules.nixvim
       ];
       extraSpecialArgs = {
         inherit pkgs-stable;
+        inherit userSettings;
+        inherit systemSettings;
+        inherit inputs;
       };
     };
   };
