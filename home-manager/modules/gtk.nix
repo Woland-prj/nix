2917d0d51d42fdea3ef623eae696e624ebeff0e3
Woland-prj <nzimin93@gmail.com>
2024-10-26 16:38:54 +0300

    gtk

--- home-manager/modules/gtk.nix
+++ home-manager/modules/gtk.nix
@@ -1,11 +1,13 @@
 {pkgs, ...}: {
   gtk = {
     enable = true;
-    theme = {
-      name = "catppuccin-GTK";
-      package = pkgs.catppuccin-gtk;
-    };
+    gtk.theme.name = "Adwaita-dark";
     iconTheme.package = pkgs.papirus-icon-theme;
     iconTheme.name = "Papirus Dark";
+  };
+  dconf.settings = {
+    "org/gnome/desktop/interface" = {
+      color-scheme = "prefer-dark";
+    };
   };
 }
