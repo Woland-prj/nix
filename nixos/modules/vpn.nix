51398c1fb036cf9c979497c564a6f6cdf837d6a5
Woland-prj <nzimin93@gmail.com>
2024-12-21 12:11:30 +0300

    vpn

--- nixos/modules/vpn.nix
+++ nixos/modules/vpn.nix
@@ -14,10 +14,10 @@
             {
                 "protocol": "vless",
                 "settings": {
-                    "v2ray": [
+                    "vnext": [
                         {
                             "address": "cz1.vpnjantit.com", // Замените на адрес вашего сервера
-                            "port": 10002,
+                            "port": 443,
                             "users": [
                                 {
                                     "id": "b8c027fd-406b-4b76-b85f-ac3fa26b9530", // Ваш UUID
@@ -39,13 +39,20 @@
     }
   '';
 in {
-  systemd.services.xray = {
-    description = "Xray Core VLESS Service";
-    wantedBy = ["multi-user.target"];
-    serviceConfig = {
-      ExecStart = "${pkgs.xray}/bin/xray -config ${xray-config}";
-      Restart = "always";
-      User = "nobody";
-    };
-  };
+  # systemd.services.xray = {
+  # description = "Xray Core VLESS Service";
+  # wantedBy = ["multi-user.target"];
+  # serviceConfig = {
+  # ExecStart = "${pkgs.xray}/bin/xray -config ${xray-config}";
+  # Restart = "always";
+  # User = "nobody";
+  # };
+  # };
+
+  environment.systemPackages = [
+    (pkgs.writeShellScriptBin "run-xray" ''
+      #!/usr/bin/env bash
+      ${pkgs.xray-core}/bin/xray -config ${xray-config}
+    '')
+  ];
 }
